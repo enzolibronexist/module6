1e4459135a229aa42a653fdcece8fbab508e74f0
Lorenzo Libron <llibron@exist.com>
2025-12-14 16:51:46 +0800

    setup persistence

--- src/main/java/com/bpi/module6/App.java
+++ src/main/java/com/bpi/module6/App.java
@@ -1,10 +1,26 @@
 package com.bpi.module6;
 
-/**
- * Hello world!
- */
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+    	EntityManager em = emf.createEntityManager();
+    	
+    	try {
+    		
+    		em.getTransaction().begin();
+    		
+    		
+    		em.getTransaction().commit();
+    	} finally {
+    		em.close();
+    		emf.close();
+    	}
+    	
     }
 }
