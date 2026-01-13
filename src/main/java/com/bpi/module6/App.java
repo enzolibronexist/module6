ed4fd8d6e1e2927714baaea2a0e415f7ee8c77ea
Lorenzo Libron <llibron@exist.com>
2026-01-13 17:02:44 +0800

    add fetching sample

--- src/main/java/com/bpi/module6/App.java
+++ src/main/java/com/bpi/module6/App.java
@@ -5,189 +5,69 @@ import java.util.List;
 
 import com.bpi.module6.model.Category;
 import com.bpi.module6.model.Product;
-import com.bpi.module6.model.ProductInventory;
-import com.bpi.module6.model.Tag;
 import com.bpi.module6.util.EntityManagerUtil;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 
-
 public class App {
-	
-    public static void main(String[] args) {
-    	
-    	EntityManager em = EntityManagerUtil.createEntityManager();
 
-    	
-    	try {
-    		
-    		runBasicPersist(em);
-    		
-    		//transaction #3 - Selecting Specific Fields
-    		em.getTransaction().begin(); //begin transaction
-    		
-    		TypedQuery<Category> query = em.createQuery("SELECT c FROM Category c WHERE c.name = :name",
-    				Category.class);
-    		
-    		query.setParameter("name", "Peripherals");
+	public static void main(String[] args) {
 
-    		Category category = query.getSingleResult();
-    		
-    		System.out.println(category.toString());
+		EntityManager em = EntityManagerUtil.createEntityManager();
 
-    		System.out.println(category.getProducts());
-    		
-    		category.getProducts().forEach(product -> System.out.println(product.getName()));
+		try {
 
-    		em.getTransaction().commit(); // commit transaction
-    		
-    		
-    	} finally {
-    		
-    		EntityManagerUtil.closeEntityManager(em);
-    		EntityManagerUtil.shutdownFactory();
-    	}
-    	
-    }
-    
-    public static void runJpqlJoin(EntityManager em) {
-		//transaction #3 - Selecting Specific Fields
-		em.getTransaction().begin(); //begin transaction
-		
-		
-		TypedQuery<Product> query = em.createQuery("SELECT p FROM Product p JOIN p.category c WHERE c.name = :categoryName",
-		        Product.class);
-		query.setParameter("categoryName", "Peripherals");
-		
+			runFetchingSample(em);
 
-		List<Product> products = query.getResultList();
-		
-		products.forEach(name -> System.out.println(name));
+		} finally {
 
-		em.getTransaction().commit(); // commit transaction
-    }
-    
-    public static void runJpqlPositionalParameters(EntityManager em) {
-		//transaction #3 - Selecting Specific Fields
-		em.getTransaction().begin(); //begin transaction
-		
-		TypedQuery<Product> query = em.createQuery("SELECT p FROM Product p WHERE p.price > ?1 AND p.brand = ?2",
-				Product.class);
-		
-		query.setParameter(1, 2000);
-		query.setParameter(2, "Logitech");
+			EntityManagerUtil.closeEntityManager(em);
+			EntityManagerUtil.shutdownFactory();
+		}
 
-		List<Product> products = query.getResultList();
-		
-		products.forEach(name -> System.out.println(name));
+	}
 
-		em.getTransaction().commit(); // commit transaction
-    }
-    
-    public static void runJpqlWhereAndNamedParameters(EntityManager em) {
-		//transaction #3 - Selecting Specific Fields
-		em.getTransaction().begin(); //begin transaction
-		
-		TypedQuery<Product> query = em.createQuery(
-		        "SELECT p FROM Product p WHERE p.name = :name",
-		        Product.class);
-		
-		query.setParameter("name", "keyboard");
+	public static void runFetchingSample(EntityManager em) {
 
-		List<Product> products = query.getResultList();
-		
-		products.forEach(name -> System.out.println(name));
+		/*
+		 * transaction #1 - save sample data
+		 */
+		em.getTransaction().begin();
 
-		em.getTransaction().commit(); // commit transaction
-    }
-    
-    public static void runJpqlSelectingSpecificFields(EntityManager em) {
-		//transaction #3 - Selecting Specific Fields
-		em.getTransaction().begin(); //begin transaction
-		
-		TypedQuery<String> getAllProductName = em.createQuery(
-		        "SELECT p.name FROM Product p",
-		        String.class);
+		Category workCategory = new Category();
+		workCategory.setName("Work");
 
-		List<String> names = getAllProductName.getResultList();
-		
-		names.forEach(name -> System.out.println(name));
+		em.persist(workCategory);
 
-		em.getTransaction().commit(); // commit transaction
-    }
-    
-    public static void runBasicPersist(EntityManager em) {
-		//transaction# 1 - persist
-		em.getTransaction().begin(); // begin transaction
-		
-		Tag gaming = new Tag();
-		gaming.setName("gaming");
-		em.persist(gaming);
-		
-		Category peripheral = new Category();
-		peripheral.setName("Peripherals");
-		
-		Product product = new Product();
-		em.persist(product);
-		
-		Product keyboard = new Product();
-		keyboard.setName("G915 Mechanical Keyboard");
-		keyboard.setPrice(new BigDecimal("3000.00"));
-		keyboard.setBrand("Logitech");
-		keyboard.setCategory(peripheral);
-		peripheral.getProducts().add(keyboard);
-		keyboard.addTag(gaming);
-		em.persist(keyboard);
-		
-		ProductInventory productInventory = new ProductInventory();
-		productInventory.setQuantity(5);
-		productInventory.setWarehouseLocation("Pasig Warehouse");
-		productInventory.setProduct(keyboard);
-		em.persist(productInventory);
-		
-		Product keyboard2 = new Product();
-		keyboard2.setName("G502");
-		keyboard2.setPrice(new BigDecimal("5000.00"));
-		keyboard2.setBrand("Logitech");
-		keyboard2.setCategory(peripheral);
-		peripheral.getProducts().add(keyboard2);
-		em.persist(keyboard2);
+		Product macbookPro = new Product();
+		macbookPro.setBrand("Apple");
+		macbookPro.setCategory(workCategory);
+		macbookPro.setName("Macbook Pro M4");
+		macbookPro.setPrice(new BigDecimal("100000.00"));
 
-		Product mouse = new Product();
-		mouse.setName("X3 CrazyLight");
-		mouse.setPrice(new BigDecimal("6000.00"));
-		mouse.setBrand("Pulsar");
-		mouse.setCategory(peripheral);
-		peripheral.getProducts().add(mouse);
-		em.persist(mouse);
+		em.persist(macbookPro);
+
+		em.getTransaction().commit();
+
+		Long macbookProId = macbookPro.getId();
+
+		/*
+		 * transaction #2 - get sample data
+		 */
+		em.getTransaction().begin();
 		
-		Product monitor = new Product();
-		monitor.setName("XL2540X");
-		monitor.setPrice(new BigDecimal("8000.00"));
-		monitor.setBrand("Zowie");
-		monitor.setCategory(peripheral);
-		peripheral.getProducts().add(monitor);
-		em.persist(monitor);
+		em.clear(); // clear persistence context
 		
-		em.persist(peripheral);
+		System.out.println("is macbookPro inside context: " + em.contains(macbookPro));
+
+		Product macbookProFromDatabase = em.find(Product.class, macbookProId);
 		
-		em.getTransaction().commit(); // commit transaction
-    }
-    
-    public static void runBasicJpql(EntityManager em) {
-		//transaction #3 - Basic JPQL
-		em.getTransaction().begin(); //begin transaction
+		Category categoryFromDatabase = macbookProFromDatabase.getCategory();
 		
-		TypedQuery<Product> getAllProducts = em.createQuery(
-		        "SELECT p FROM Product p",
-		        Product.class);
-		
-		List<Product> products = getAllProducts.getResultList();
-		
-		products.forEach(product -> System.out.println(product.toString()));
-		
-		
-		em.getTransaction().commit(); //commit transaction
-    }
+		System.out.println(categoryFromDatabase.toString());
+
+		em.getTransaction().commit();
+
+	}
 }
--- src/main/java/com/bpi/module6/model/Product.java
+++ src/main/java/com/bpi/module6/model/Product.java
@@ -7,14 +7,12 @@ import java.util.Set;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,22 +32,10 @@ public class Product {
 	@Column
 	private String brand;
 	
-    @ManyToMany
-    @JoinTable(
-        name = "product_tag",
-        joinColumns = @JoinColumn(name = "product_id"),
-        inverseJoinColumns = @JoinColumn(name = "tag_id")
-    )
-    private Set<Tag> tags = new HashSet<>();
-	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "category_id")
 	private Category category;
 	
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "product")
-    private ProductInventory inventory;
-    
-
 	
 	public Long getId() {
 		return id;
@@ -90,33 +76,6 @@ public class Product {
 
 	public void setCategory(Category category) {
 		this.category = category;
-	}
-
-	public ProductInventory getInventory() {
-		return inventory;
-	}
-
-	public void setInventory(ProductInventory inventory) {
-		this.inventory = inventory;
-	}
-	
-    // helper methods
-    public void addTag(Tag tag) {
-        tags.add(tag);
-        tag.getProducts().add(this);
-    }
-
-    public void removeTag(Tag tag) {
-        tags.remove(tag);
-        tag.getProducts().remove(this);
-    }
-
-	public Set<Tag> getTags() {
-		return tags;
-	}
-
-	public void setTags(Set<Tag> tags) {
-		this.tags = tags;
 	}
 
 	@Override
